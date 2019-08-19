8a97688244dd87b22824fd6e97e7823837b1d6bc
Simon Hofmann <simon.hofmann@consol.de>
2019-08-19 09:33:22 +0200

    Updated Sakuli install script

--- src/ubuntu/install/sakuli.sh
+++ src/ubuntu/install/sakuli.sh
@@ -8,8 +8,19 @@ source $HOME/.bashrc
 echo "nvm use $1"
 nvm use $1
 
+echo "Configuring .npmrc"
+echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" > $HOME/.npmrc
+npm whoami
+
 echo "Installing Sakuli v$2"
 npm i -g @sakuli/cli@$2
+
+forwarders=("gearman" "checkmk" "icinga2")
+for fwd in "${forwarders[@]}"
+do
+    echo "Installing Sakuli forwarder $fwd"
+    npm i -g @sakuli/forwarder-$fwd
+done
 
 echo "Installing ChromeDriver v$3"
 npm i -g chromedriver@$3
