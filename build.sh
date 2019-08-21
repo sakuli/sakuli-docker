5a1810e865b557efb2278996182a1420b9c1ce49
Simon Hofmann <simon.hofmann@consol.de>
2019-08-21 16:19:31 +0200

    Updated run script

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ docker build \
 --build-arg=CHROMIUM_VERSION=76.0.3809.100-0ubuntu0.18.04.1 \
 --build-arg=CHROMEDRIVER=76 \
 --build-arg=GECKODRIVER=1.16.2 \
---build-arg=NPM_ACCESS_TOKEN=
+--build-arg=NPM_ACCESS_TOKEN=b7fc5d41-1dbe-49f6-ba30-d74682ee0b6a
