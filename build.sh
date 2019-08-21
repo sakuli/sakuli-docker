ff2d0354195e7a2958cbf11d980ef0829846bd44
Simon Hofmann <simon.hofmann@consol.de>
2019-08-21 16:17:33 +0200

    Updated tag in build script

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 docker build \
--t consol/sakuli:2.1.2 \
+-t taconsol/sakuli:2.1.2 \
 -f Dockerfile.sakuli-ubuntu-xfce . \
 --build-arg=SAKULI_VERSION=2.1.2 \
 --build-arg=FIREFOX_VERSION=68.0.2+build1-0ubuntu0.18.04.1 \
 --build-arg=CHROMIUM_VERSION=76.0.3809.100-0ubuntu0.18.04.1 \
 --build-arg=CHROMEDRIVER=76 \
---build-arg=LICENSE_TOKEN=
---build-arg=NPM_TOKEN=
+--build-arg=GECKODRIVER=1.16.2 \
+--build-arg=NPM_ACCESS_TOKEN=
