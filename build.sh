bd330b6527867434f3f5e1514a51ac6d9669f426
Sven Hettwer <sven.hettwer@consol.de>
2019-09-17 12:07:17 +0200

    Updated Firefox

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ docker build \
 -t taconsol/sakuli:2.1.2 \
 -f Dockerfile.sakuli-ubuntu-xfce . \
 --build-arg=SAKULI_VERSION=2.1.2 \
---build-arg=FIREFOX_VERSION=68.0.2+build1-0ubuntu0.18.04.1 \
+--build-arg=FIREFOX_VERSION=69.0+build2-0ubuntu0.18.04.1 \
 --build-arg=CHROMIUM_VERSION=76.0.3809.100-0ubuntu0.18.04.1 \
 --build-arg=CHROMEDRIVER=76 \
 --build-arg=GECKODRIVER=1.16.2 \
