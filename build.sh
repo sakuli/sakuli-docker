1a872a9b31783cd36124e9228876922e93bf94ad
Sven Hettwer <sven.hettwer@consol.de>
2019-09-17 14:42:54 +0200

    Deleted CHROMEDRIVER and GECKODRIVER from buildargs

--- build.sh
+++ build.sh
@@ -6,6 +6,4 @@ docker build \
 --build-arg=SAKULI_VERSION=2.1.3 \
 --build-arg=FIREFOX_VERSION=69.0+build2-0ubuntu0.18.04.1 \
 --build-arg=CHROMIUM_VERSION=76.0.3809.100-0ubuntu0.18.04.1 \
---build-arg=CHROMEDRIVER=76 \
---build-arg=GECKODRIVER=1.16.2 \
 --build-arg=NPM_ACCESS_TOKEN=b7fc5d41-1dbe-49f6-ba30-d74682ee0b6a
