19c2fff8a78d48208652fe89334da784988bd6d6
Simon Hofmann <simon.hofmann@consol.de>
2019-09-17 13:58:30 +0200

    Removed webdriver npm packages and yargs

--- src/ubuntu/install/sakuli.sh
+++ src/ubuntu/install/sakuli.sh
@@ -8,20 +8,11 @@ source $HOME/.bashrc
 echo "nvm use $1"
 nvm use $1
 
-echo "//registry.npmjs.org/:_authToken=$5" > $HOME/.npmrc
+echo "//registry.npmjs.org/:_authToken=$3" > $HOME/.npmrc
 npm whoami
 
 echo "Installing Sakuli v$2"
 npm i -g @sakuli/cli@$2
-
-echo "Installing ChromeDriver v$3"
-npm i -g chromedriver@$3
-
-echo "Installing GeckoDriver v$4"
-npm i -g geckodriver@$4
-
-echo "Installing yargs"
-npm i -g yargs
 
 forwarders=("gearman" "checkmk" "icinga2")
 for fwd in "${forwarders[@]}"
@@ -32,9 +23,3 @@ done
 
 echo "Configuring .npmrc"
 echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" > $HOME/.npmrc
-
-# The following step is currently required, since /usr/bin/env doesn't handle arguments properly on Linux.
-# As @sakuli/cli contains the "#!/usr/bin/env node --no-warnings" shebang, it fails to start
-# This temporary workaround updates the shebang line to mitigate this error
-# echo "Patching Sakuli bin"
-# sed -i '1s/.*/#!\/usr\/bin\/env node/' $HOME/.nvm/versions/node/v10.16.0/lib/node_modules/@sakuli/cli/dist/index.js
