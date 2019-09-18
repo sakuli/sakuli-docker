9e4ec1be10be63f4729d7a278c559368df55592f
Simon Hofmann <simon.hofmann@consol.de>
2019-09-18 11:07:38 +0200

    Added license generator source

--- src/ubuntu/install/firefox.sh
+++ src/ubuntu/install/firefox.sh
@@ -3,6 +3,7 @@ set -e
 
 echo "Install Firefox v$1"
 apt-get update -y
-apt-get install -y firefox=$1
+apt-get install -y firefox=$1 firefox-geckodriver=$1
 apt-get clean -y
 apt-get autoremove -y
+rm -rf /var/lib/apt/lists/*
