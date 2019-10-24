0ac713dbbcbf698df431cbb8b13b3de7c53be950
Simon Hofmann <simon.hofmann@consol.de>
2019-10-24 14:38:38 +0200

    Removed firefox and chromium build args, since Ubuntu repos do not keep a package history

--- build.sh
+++ build.sh
@@ -7,11 +7,9 @@ cp -rf ./license-validator/dist/index.js ./src/common/env/startup.js
 cp -rf ./license-validator/node_modules ./src/common/env/node_modules
 
 docker build \
--t taconsol/sakuli:2.1.3 \
+-t taconsol/sakuli-openbox:2.1.3 \
 -f Dockerfile.sakuli-ubuntu-xfce . \
 --build-arg=SAKULI_VERSION=2.1.3 \
---build-arg=FIREFOX_VERSION=69.0+build2-0ubuntu0.18.04.1 \
---build-arg=CHROMIUM_VERSION=76.0.3809.100-0ubuntu0.18.04.1 \
 --build-arg=NPM_ACCESS_TOKEN=b7fc5d41-1dbe-49f6-ba30-d74682ee0b6a
 
 cd ./.test/
