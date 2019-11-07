88f996aa8637cd85f6ef84ad7fb1ec3b4646eaa1
Simon Hofmann <simon.hofmann@consol.de>
2019-11-07 15:40:07 +0100

    Updated image name

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ cp -rf ./license-validator/dist/index.js ./src/common/env/startup.js
 cp -rf ./license-validator/node_modules ./src/common/env/node_modules
 
 docker build \
--t taconsol/sakuli-openbox:2.1.3 \
+-t taconsol/sakuli:2.1.3 \
 -f Dockerfile.sakuli-ubuntu-openbox . \
 --build-arg=SAKULI_VERSION=2.1.3 \
 --build-arg=NPM_ACCESS_TOKEN=b7fc5d41-1dbe-49f6-ba30-d74682ee0b6a
