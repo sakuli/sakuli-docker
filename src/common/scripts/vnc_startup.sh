92e441f73516e820b79dda8d0d7a65a076773105
Simon Hofmann <simon.hofmann@consol.de>
2019-11-07 15:37:52 +0100

    Updated image tags

--- src/common/scripts/vnc_startup.sh
+++ src/common/scripts/vnc_startup.sh
@@ -9,14 +9,14 @@ USAGE:
 docker run -it -p 6901:6901 -p 5901:5901 consol/<image>:<tag> <option>
 
 IMAGES:
-consol/sakuli
+taconsol/sakuli
 
 OPTIONS:
 -w, --wait      (default) keeps the UI and the vncserver up until SIGINT or SIGTERM will received
 -s, --skip      skip the vnc startup and just execute the assigned command.
-                example: docker run consol/centos-xfce-vnc --skip bash
+                example: docker run taconsol/sakuli --skip bash
 -d, --debug     enables more detailed startup output
-                e.g. 'docker run consol/centos-xfce-vnc --debug bash'
+                e.g. 'docker run taconsol/sakuli --debug bash'
 -h, --help      print out this help
 "
 }
