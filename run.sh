ca70664a36b6cf0f3daae7bb08cce3b12ed8317f
Simon Hofmann <simon.hofmann@consol.de>
2019-11-07 15:40:32 +0100

    Updated image name in run script

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 docker run \
     --rm \
@@ -6,4 +6,4 @@ docker run \
     -e SAKULI_LICENSE_KEY= \
     -p 5902:5901 \
     -p 6902:6901 \
-    taconsol/sakuli-openbox:2.1.3
+    taconsol/sakuli:2.1.3
