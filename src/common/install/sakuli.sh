60616f67317ddbf9565f44cdf41a34a9fdabc2c2
Sven Hettwer <sven.hettwer@consol.de>
2020-06-25 11:26:06 +0200

    (#47) Added typescript and sakuli legacy types to container

--- src/common/install/sakuli.sh
+++ src/common/install/sakuli.sh
@@ -12,14 +12,19 @@ echo "//registry.npmjs.org/:_authToken=$3" > $HOME/.npmrc
 npm whoami
 
 echo "Installing Sakuli v$2"
-npm i -g @sakuli/cli@$2
+npm i -g -E @sakuli/cli@$2
 
 forwarders=("gearman" "checkmk" "icinga2" "prometheus")
 for fwd in "${forwarders[@]}"
 do
     echo "Installing Sakuli forwarder $fwd v$2"
-    npm i -g @sakuli/forwarder-$fwd@$2
+    npm i -g -E @sakuli/forwarder-$fwd@$2
 done
+
+echo "Installing Typescript"
+npm i -g -E typescript@3.9.5
+echo "Installing sakuli legacy types"
+npm i -g -E @sakuli/legacy-types@$2
 
 echo "Configuring .npmrc"
 echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" > $HOME/.npmrc
