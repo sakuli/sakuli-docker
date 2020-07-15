b1401aaab0b4b5196697b28729e550c3e773d70f
Simon Hofmann <simon.hofmann@consol.de>
2020-07-15 14:25:22 +0200

    (#53) Added global installs for @sakuli/legacy, @sakuli/legacy-types and typescript to work with autodiscovery

--- src/common/install/sakuli.sh
+++ src/common/install/sakuli.sh
@@ -14,12 +14,21 @@ npm whoami
 echo "Installing Sakuli v$2"
 npm i -g @sakuli/cli@$2
 
+echo "Installing Sakuli legacy v$2"
+npm i -g @sakuli/legacy@$2
+
+echo "Installing Sakuli legacy-types v$2"
+npm i -g @sakuli/legacy-types@$2
+
 forwarders=("gearman" "checkmk" "icinga2" "prometheus")
 for fwd in "${forwarders[@]}"
 do
     echo "Installing Sakuli forwarder $fwd v$2"
     npm i -g @sakuli/forwarder-$fwd@$2
 done
+
+echo "Installing TypeScript v3.8.3"
+npm i -g -E typescript@3.8.3
 
 echo "Configuring .npmrc"
 echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" > $HOME/.npmrc
