b671f6e6ec02a4453a1bbbffdeb1d6c0f891a114
Van Cliff Le <cliff.le.van@consol.de>
2020-08-04 12:36:06 +0200

    (#19) added tests

--- .test/test.sh
+++ .test/test.sh
@@ -22,3 +22,11 @@ docker run \
     -u 45678:12345 \
     --shm-size=2G \
     taconsol/sakuli:${1:-latest}
+
+docker run \
+    --rm \
+    -e SAKULI_LICENSE_KEY=${SAKULI_LICENSE_KEY} \
+    -e SAKULI_TEST_SUITE=/headless/git-repository/packages/e2e/ \
+    -e GIT_URL=https://github.com/sakuli/sakuli.git \
+    --shm-size=2G \
+    taconsol/sakuli:${1:-latest}
