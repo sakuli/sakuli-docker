3937f54d7e8217e6a21c93dc97e264f6ef8d7e05
Van Cliff Le <cliff.le.van@consol.de>
2020-08-05 10:04:49 +0200

    (#19) updated demo testcase

--- src/demo/suite/test/sakuli_io.js
+++ src/demo/suite/test/sakuli_io.js
@@ -2,9 +2,9 @@
   const testCase = new TestCase("sakuli.io", 60, 70);
 
   try {
-    await _navigateTo("https://sakuli.io");
-    await testCase.endOfStep("Open Landing Page", 8, 10);
-    await _click(_link("Getting started"));
+    await _navigateTo("https://sakuli.io/docs");
+    await testCase.endOfStep("Open Sakuli Docs", 8, 10);
+    await _click(_link(/Getting Started/));
     await testCase.endOfStep("Navigate to Getting Started", 3, 5);
     await _highlight(_code("npm init"));
     await testCase.endOfStep("Sakuli Landing Page", 10);
@@ -13,4 +13,4 @@
   } finally {
     testCase.saveResult();
   }
-})().then(done);
+})();
