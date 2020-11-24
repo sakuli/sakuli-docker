09afff0a1b9ddde3f59e54836bcb40c7762a5f01
Sven Hettwer <sven.hettwer@consol.de>
2020-11-24 08:58:02 +0100

    fixed test due to structural change in sakuli repo

--- .test/test.sh
+++ .test/test.sh
@@ -40,7 +40,7 @@ docker run \
     --rm \
     -e SAKULI_LICENSE_KEY=${SAKULI_LICENSE_KEY} \
     -e GIT_URL=https://github.com/sakuli/sakuli.git \
-    -e GIT_CONTEXT_DIR=packages/e2e/e2e-suite \
+    -e GIT_CONTEXT_DIR=packages/e2e/ \
     --shm-size=2G \
     taconsol/sakuli:${1:-latest}
 
