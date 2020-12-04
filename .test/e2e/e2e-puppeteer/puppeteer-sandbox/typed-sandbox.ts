850a5c5b54bd581036a8ca663924bd268bb8310c
Van Cliff Le <cliff.le.van@consol.de>
2020-12-04 12:34:52 +0100

    (#48) added test with puppeteer

---
+++ .test/e2e/e2e-puppeteer/puppeteer-sandbox/typed-sandbox.ts
@@ -0,0 +1,27 @@
+(async () => {
+  const tc = new TestCase('sandbox-ts', 100, 200);
+  const puppeteer = require('puppeteer');
+  const env = new Environment();
+
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: {width: 1000, height: 1000}
+  });
+
+  try {
+    const page = await browser.newPage();
+    await page.goto('https://sakuli.io/e2e-pages/sandbox/');
+
+    const frames =  page.frames()[0].childFrames();
+    //click license information
+    await frames[0].click('body > nav > a');
+    //click go back to fried egg
+    await frames[0].click('body > nav > a');
+    await env.sleep(1)
+  } catch (e) {
+    await tc.handleException(e);
+  } finally {
+    await tc.saveResult();
+    await browser.close();
+  }
+})();
