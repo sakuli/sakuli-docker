61e4e544f76ecad7491c7239e339f14743c47d1d
Van Cliff Le <cliff.le.van@consol.de>
2020-12-07 13:52:12 +0100

    (#48) add timeout after navigating to new page

--- .test/e2e/e2e-puppeteer/puppeteer-sandbox/typed-sandbox.ts
+++ .test/e2e/e2e-puppeteer/puppeteer-sandbox/typed-sandbox.ts
@@ -3,23 +3,25 @@
   const puppeteer = require('puppeteer');
   const env = new Environment();
 
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
+
   try {
-    const browser = await puppeteer.launch({
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
-    });
     const page = await browser.newPage();
     await page.goto('https://sakuli.io/e2e-pages/sandbox/');
 
     const frames = page.frames()[0].childFrames();
     //click license information
     await frames[0].click('body > nav > a');
+    await frames[0].waitForNavigation();
     //click go back to fried egg
     await frames[0].click('body > nav > a');
     await env.sleep(1)
-    await browser.close();
   } catch (e) {
     await tc.handleException(e);
   } finally {
+    await browser.close();
     await tc.saveResult();
   }
 })();
