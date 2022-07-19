87513aceb4036f78fdea744ad194e51bd50bf3d6
Van Cliff Le <cliff.le.van@consol.de>
2022-07-19 14:43:43 +0200

    chore: fixed GitHub download link for nvm in build script

--- src/common/install/nvm.sh
+++ src/common/install/nvm.sh
@@ -2,7 +2,7 @@
 ### every exit != 0 fails the script
 set -e
 
-NVM_VERSION=$(curl -s https://api.github.com/repos/novnc/novnc/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+NVM_VERSION=$(curl -s https://api.github.com/repos/nvm-sh/nvm/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 echo NVM_VERSION=$NVM_VERSION
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/$NVM_VERSION/install.sh | bash
 
